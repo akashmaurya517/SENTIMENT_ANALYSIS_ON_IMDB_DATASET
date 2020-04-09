627d46cd438cbc313f8b3b034f3db64b39bc2aad
Akash maurya <44486527+akashmaurya517@users.noreply.github.com>
2020-04-09 16:13:22 +0530

    Add files via upload

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,33 @@
+import keras
+import numpy as np
+from keras.models import load_model
+from keras.datasets import imdb
+
+
+#dictoinery
+word_to_id = keras.datasets.imdb.get_word_index()
+#loading the model ***
+model = load_model('Your_Model.h5') #instead you can use pretrained model just replace the name and put the name of the model which i have made and saved in the directory
+
+a = input("Enter a review\n")
+
+b = []
+for i in a.split():
+  try:
+    b.append(word_to_id[i])
+  except:
+    pass
+
+b = np.array(b)
+
+def multi_hot_encode(sequences, dimension):
+  
+  results = np.zeros(dimension)
+  for i in range(len(sequences)):
+  	results[i] = 1
+  return results
+
+validation = multi_hot_encode(b, 10000)
+validation = np.array([validation])
+print("Negative, Positive")
+print(model.predict(validation))
